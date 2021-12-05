85def3182bcb489cf6064eab30f69ee97905b36e
cgrillo-bit <81266006+cgrillo-bit@users.noreply.github.com>
2021-12-05 03:59:45 -0500

    scope fixes

--- crm/leads/admin.py
+++ crm/leads/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import User, Lead, AE
+from .models import User, Lead, Account_exec
 
 admin.site.register(User)
+admin.site.register(Lead)
+admin.site.register(Account_exec)
