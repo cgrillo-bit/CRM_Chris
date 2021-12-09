7b017f048aae1953759c1a960fadb0d9c30a0c07
cgrillo-bit <81266006+cgrillo-bit@users.noreply.github.com>
2021-12-09 00:28:10 -0500

    fixed styling

--- crm/core_crm/settings.py
+++ crm/core_crm/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_tailwind',
     'leads',
     'opportunities',
+    
 ]
 
 MIDDLEWARE = [
@@ -132,3 +135,7 @@ STATIC_ROOT = "root_static"
 AUTH_USER_MODEL = 'leads.User'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 LOGIN_REDIRECT_URL = "/leads"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
